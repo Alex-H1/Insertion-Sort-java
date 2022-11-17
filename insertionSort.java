f3a8980f6c668b6fe132c5f50f6bba149496c2f3
alex hernandez <alexanderohernandez21@gmail.com>
2022-11-17 12:27:57 -0800

    testing

--- insertionSort.java
+++ insertionSort.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 class InsertionSort {
     public final static void main(String args[]) {
-        int[] arr1 = {10, 14, 28, 11, 7, 16, 30, 50, 25, 18};
-        int[] arr2 = {7, 10, 11, 14, 16, 18, 25, 28, 30, 50, 77, 32, 47, 2, 88, 42, 7};
-
+        int[] arr1 = {10, 14, 28, 11, 7, 16, 30, 50, 25, 18, 2, 1, 77, 88, 90, 54, 66, 77, 8, 9, 22, 26, -1};
         List<Integer> sortedArray = new ArrayList<Integer>();
 
         // we mark the first number as already sorted
@@ -14,13 +12,12 @@ class InsertionSort {
         for (int number : arr1) {
             i++;
 //            //get next number from unsorted array and compares to the last number of sorted array
-            if (arr1[i] > sortedArray.get(sortedArray.size() - 1)){
+            if (arr1[i] > sortedArray.get(sortedArray.size() - 1)) {
                 sortedArray.add(arr1[i]);
-            }else{
-
-                for(int j = 0; j < sortedArray.size(); j++)
+            } else {
+                for (int j = 0; j < sortedArray.size(); j++) {
                     //if next number is not sorted go back and find placement for placement
-                    if(arr1[i] < sortedArray.get(j)){
+                    if (arr1[i] < sortedArray.get(j)) {
                         //add number to specified index
                         sortedArray.add(j, arr1[i]);
                         break;
@@ -29,10 +26,10 @@ class InsertionSort {
             }
 
             //error handling
-            if(i == arr1.length - 1){
+            if (i == arr1.length - 1) {
                 break;
             }
+            System.out.println(sortedArray);
         }
-        System.out.println(sortedArray);
     }
 }
