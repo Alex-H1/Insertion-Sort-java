e66f673ff7d7e9db00000208c5f2fa2d54ba1120
alex-h <alexanderohernandez21@gmail.com>
2022-11-17 16:16:55 -0800

    refactor

--- insertionSort.java
+++ insertionSort.java
@@ -3,15 +3,13 @@ import java.util.*;
 class InsertionSort {
     public final static void main(String args[]) {
         int[] arr1 = {10, 14, 28, 11, 7, 16, 30, 50, 25, 18, 2, 1, 77, 88, 90, 54, 66, 77, 8, 9, 22, 26, -1};
+        //instantiate mutable list
         List<Integer> sortedArray = new ArrayList<Integer>();
-
         // we mark the first number as already sorted
         sortedArray.add(arr1[0]);
 
-        int i = 0;
-        for (int number : arr1) {
-            i++;
-//            //get next number from unsorted array and compares to the last number of sorted array
+        for (int i = 0; i < arr1.length; i++) {
+           //get next number from unsorted array and compares to the last number of sorted array
             if (arr1[i] > sortedArray.get(sortedArray.size() - 1)) {
                 sortedArray.add(arr1[i]);
             } else {
